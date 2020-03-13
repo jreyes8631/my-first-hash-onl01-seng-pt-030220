a8ea55d0496f760618de131172122d6e85051c15
Jose E Reyes <jreyes8631@gmail.com>
2020-03-13 23:53:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,7 +31,10 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+def adding
+  shipping_manifest = {
+                  ""
+  }
 
 
   # add 4 gun powder to the shipping_manifest hash below
