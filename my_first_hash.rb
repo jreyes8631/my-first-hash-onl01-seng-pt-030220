e7e8720817bcbc19016c21d76f16c40ae70ab099
Jose E Reyes <jreyes8631@gmail.com>
2020-03-14 00:35:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,28 +19,13 @@ shipping_manifest = {
 end
 
 def retrieval
-   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-                
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-hash = {"oil paintings" => 3}
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3,
-                   "muskets" => 2,
-                   "gun powder" => 4
-                 }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
 end
