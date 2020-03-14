6582dddb7d16dd97f3a5fc26a0652957c3b95e29
Jose E Reyes <jreyes8631@gmail.com>
2020-03-14 00:34:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,9 +20,7 @@ end
 
 def retrieval
    shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  shipping_manifest["oil paintings"]
-                 
-
+                
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 hash = {"oil paintings" => 3}
 end
