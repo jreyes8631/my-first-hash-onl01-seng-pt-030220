470bcd710b9aba7f8211b888c2327a0f9036c808
Jose E Reyes <jreyes8631@gmail.com>
2020-03-14 00:37:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,6 +24,10 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = 
+  shipping_manifest = {"whale bone corsets" => 5,
+"porcelain vases" => 2,
+"oil paintings" => 3
+"muskets" => 2
+"gun powder" => 4}
   
 end
